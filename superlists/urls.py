ce2e18e1997c8bce735bed0cb31a76a887f54da6
Andre Bach <nonnormalizable@gmail.com>
2014-09-14 16:42:45 -0700

    New URL and view for adding item to existing list; unit tests. FT passes.

--- superlists/urls.py
+++ superlists/urls.py
@@ -11,9 +11,14 @@ urlpatterns = patterns('',
         name='home'
         ),
     url(
-        r'^lists/the-only-list-in-the-world/$',
+        r'^lists/(\d+)/$',
         'lists.views.view_list',
         name='view_list'
+        ),
+    url(
+        r'^lists/(\d+)/add_item$',
+        'lists.views.add_item',
+        name='add_item'
         ),
     url(
         r'^lists/new$',
